74a9fdb22c4c666c4dbc0448b644c72a54a790f0
LEGENwait <156962324+LEGENwait@users.noreply.github.com>
2024-06-24 15:43:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,35 @@
 ﻿using System;
-
-namespace Practice_5_1
+namespace Practice_5_2
 {
     class Program
     {
         static void Main()
         {
-            Console.Write("Введите ваше предложение: ");
+            Console.Write("Введите своё предложение: ");
             string inputText = Console.ReadLine();
-            SplitText(inputText);
+            Console.WriteLine(ReverseString(inputText));
         }
-        static string SplitText(string text)
+
+        static string ReverseString(string inputText)
+        {
+            string[] stringParts =  SplitText(inputText) ;
+            string resultString = "";
+
+            for (int i = stringParts.Length -1; i >= 0; i--)
+            {
+                resultString += stringParts[i] + " ";
+            }
+            return resultString;
+        }
+        static string[] SplitText(string text)
         {
             string someText = text;
             string[] subs = someText.Split(' ');
             foreach (string sub in subs)
             {
-                Console.WriteLine(sub);
+                return subs;
             }
-            return someText;
+            return subs;
         }
     }
 }
