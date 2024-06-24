036a4bcafa1bf11a95658e77288b5e1f37c1199c
LEGENwait <156962324+LEGENwait@users.noreply.github.com>
2024-06-24 15:47:27 +0300

    Add files via upload

---
+++ Practice_5_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Practice_5_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите своё предложение: ");
+            string inputText = Console.ReadLine();
+            Console.WriteLine(ReverseString(inputText));
+        }
+
+        static string ReverseString(string inputText)
+        {
+            string[] stringParts =  SplitText(inputText) ;
+            string resultString = "";
+
+            for (int i = stringParts.Length -1; i >= 0; i--)
+            {
+                resultString += stringParts[i] + " ";
+            }
+            return resultString;
+        }
+        static string[] SplitText(string text)
+        {
+            string someText = text;
+            string[] subs = someText.Split(' ');
+            foreach (string sub in subs)
+            {
+                return subs;
+            }
+            return subs;
+        }
+    }
+}
