d805f5d0768aa7fe576ca4fdcb33cc6244bb3b69
LEGENwait <156962324+LEGENwait@users.noreply.github.com>
2024-06-24 15:35:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_5_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите ваше предложение: ");
+            string inputText = Console.ReadLine();
+            SplitText(inputText);
+        }
+        static string SplitText(string text)
+        {
+            string someText = text;
+            string[] subs = someText.Split(' ');
+            foreach (string sub in subs)
+            {
+                Console.WriteLine(sub);
+            }
+            return someText;
+        }
+    }
+}
