866206e19fbc541e29c096df2ac5ed639f5c0d00
Dmitriy Kroshka <dmitrikkroshka@gmail.com>
2024-07-12 01:25:02 +0300

    Добавление комментариев

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ namespace Practice_5_1
             string inputText = Console.ReadLine();
             SplitText(inputText);
         }
-        static string SplitText(string text)
+        
+        static void SplitText(string text) //void, так как мы производим преобразование введённого текста
         {
-            string someText = text;
-            string[] subs = someText.Split(' ');
-            for (int i = 0; i < subs.Length; i++)
+            string someText = text; //создаём переменную внутри метода с переданным текстом
+            string[] subs = someText.Split(' '); //создаём массив строк из введённого текста с разделителем в виде пробела
+            for (int i = 0; i < subs.Length; i++) //перебираем массив строк
             {
-                Console.WriteLine(subs[i]);
+                Console.WriteLine(subs[i]);  //выводим каждый элемент массива в отдельной строке
             }
-            return someText;
+            //return someText; return не нужен, так как метод
+                                //производит манипуляции с переданными данными
         }
     }
 }
